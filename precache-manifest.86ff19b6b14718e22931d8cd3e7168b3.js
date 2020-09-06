b50e4a9fee595b8af9d55aaec9075339483c32e9
cap.Antonio <cheremnykh.anton@gmail.com>
2020-09-06 11:12:01 +0200

    Updates

---
+++ precache-manifest.86ff19b6b14718e22931d8cd3e7168b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c6857262af2f9be3f26896a14c3bc8",
+    "url": "/form-builder/index.html"
+  },
+  {
+    "revision": "cc43197e827ff32d1585",
+    "url": "/form-builder/static/css/main.56565ce7.chunk.css"
+  },
+  {
+    "revision": "a4573e306031d160b66c",
+    "url": "/form-builder/static/js/2.18831062.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/form-builder/static/js/2.18831062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc43197e827ff32d1585",
+    "url": "/form-builder/static/js/main.ee0bc9b4.chunk.js"
+  },
+  {
+    "revision": "63e2880f1c5187bbe06d",
+    "url": "/form-builder/static/js/runtime-main.75df113d.js"
+  }
+]);
